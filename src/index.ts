bcf92fbe729ead72caffe2d392bdb3b10c8c4bea
Martin B <porkus1990@googlemail.com>
2022-07-11 15:51:35 +0000

    chore: add description

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import * as readline from 'readline';
 import { ReadJson } from './read.json/ReadJson';
 import { SyncDeps } from './sync-deps/SyncDeps';
 
-var rl = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
   terminal: false,
 });
+
+console.log('\x1b[36m%s\x1b[0m', 'specify path to packages folder and optional a prefix like @myPackage/ < slash is required. e.g. /full-path/packages @prefix/');
 
 rl.on('line', async function (line) {
   const [path, prefix] = line.split(' ');
