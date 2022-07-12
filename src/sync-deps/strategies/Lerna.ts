588119a99701e96330fa9c48340a745329acd4e9
Martin B <porkus1990@googlemail.com>
2022-07-12 21:48:51 +0000

    chore: refactoring

--- src/sync-deps/strategies/Lerna.ts
+++ src/sync-deps/strategies/Lerna.ts
@@ -4,7 +4,7 @@ import { IStrategy } from './stretegy.interface';
 class Lerna implements IStrategy {
 
   public run(handleContent: IHandleContent): Promise<boolean> {
-    handleContent.handle({ 'sample': 'sample' }, { 'sample': 'sample' }, null);
+    handleContent.handle({ dependencies: { 'sample': 'sample' }, name: 'sample', peerDependencies: {} }, { dependencies: { 'sample': 'sample' }, name: 'sample', peerDependencies: {} }, null);
     return Promise.resolve(true);
   }
 }
